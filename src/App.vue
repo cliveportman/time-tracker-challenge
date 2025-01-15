361143f5bf33e1a25e5f5c2dce500639cf9552b7
Clive Portman <clive@theportman.co>
2025-01-15 15:34:12 +0000

    batman

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header class="bg-slate-900 text-white">
+    <img alt="Vue logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
